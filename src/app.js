f953c613d1f9341f93ead2197a4416146039d960
LNCE21 <lautaronicolascc@gmail.com>
2021-07-15 17:36:14 -0300

    Added a hamburguer menu and configure it to appear when less than 1000px

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const hamburgerButton = document.getElementById('hamburger') /*First, grab the element with id of hamburger, and after, save as a button*/
+const navList = document.getElementById('nav-list') /* Now we grab our nav elements  */
+
+function toggleButton(){                /*Toggle function, grabs the navList, classList and we want a toggle with a future class (show)*/
+    navList.classList.toggle('show')
+}
+
+/*Now we attached this to the hamburger button */
+hamburgerButton.addEventListener('click', toggleButton) /*So, if we click, we toggle the button*/
