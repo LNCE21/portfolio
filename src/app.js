7617663dd52c046ee9eca405a0bff41a2e20870e
LNCE21 <lautaronicolascc@gmail.com>
2021-08-03 23:39:46 -0300

    Commenting

--- src/app.js
+++ src/app.js
@@ -8,8 +8,7 @@ function toggleButton(){                /*Toggle function, grabs the navList, cl
 /*Now we attached this to the hamburger button */
 hamburgerButton.addEventListener('click', toggleButton) /*So, if we click, we toggle the button*/
 
-/*Here start the SVG*/
-/*We need to know the length of the path*/
+/*Here start the SVG. We need to know the length of the path*/
 const developer = document.querySelectorAll('.developer path'); /*Here we are selecting all the paths in the word*/
 for(let i = 0; i<developer.length; i++){
     console.log(`Letter ${i} is ${developer[i].getTotalLength()}`); /*The total length. Now we have to go to "inspect" and on "console"  to get the exact length*/
